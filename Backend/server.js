e5eb12a7accb9742b9a54996c0b11e266aeda9c6
shashwatk012 <shashwatkesharwani.12@gmail.com>
2025-09-27 16:24:24 +0530

    some change

--- Backend/server.js
+++ Backend/server.js
@@ -30,7 +30,7 @@ const clients = new Map();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   })
 );
